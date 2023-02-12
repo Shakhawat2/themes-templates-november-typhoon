9ae55f34f749f31ec26f9d41931dc3019b356833
Shakhawat2 <shohugmahbub@gmail.com>
2023-02-12 14:56:54 +0600

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -2,11 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" data-theme="light">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <Main/>
+        <NextScript/>
       </body>
     </Html>
   )
